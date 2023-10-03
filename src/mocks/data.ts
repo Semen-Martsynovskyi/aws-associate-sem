b88ca2fc1c3ed333da30af9c9860b35e97fbf0b3
Semen Martsynovskyi <semen_martsynovskyi@epam.com>
2023-10-04 00:25:06 +0300

    task-2 conf(serverless): configured

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -8,37 +8,37 @@ export const products: Product[] = [
     description: "Short Product Description1",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
     price: 24,
-    title: "ProductOne",
+    title: "Wakeboard Nitro",
   },
   {
     description: "Short Product Description7",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
     price: 15,
-    title: "ProductTitle",
+    title: "Wakeboard Liquid Power",
   },
   {
     description: "Short Product Description2",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
     price: 23,
-    title: "Product",
+    title: "Wakeboard Force",
   },
   {
     description: "Short Product Description4",
     id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
     price: 15,
-    title: "ProductTest",
+    title: "Wakeboard AquaLite",
   },
   {
     description: "Short Product Descriptio1",
     id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
     price: 23,
-    title: "Product2",
+    title: "Wakeboard Liquid Power CQ",
   },
   {
     description: "Short Product Description7",
     id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
     price: 15,
-    title: "ProductName",
+    title: "Wakeboard AquaLite True",
   },
 ];
 
@@ -49,10 +49,10 @@ export const availableProducts: AvailableProduct[] = products.map(
 export const cart: CartItem[] = [
   {
     product: {
-      description: "Short Product Description1",
-      id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
-      price: 24,
-      title: "ProductOne",
+      description: "Short Product Descriptio1",
+      id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+      price: 23,
+      title: "Wakeboard Liquid Power CQ",
     },
     count: 2,
   },
@@ -61,7 +61,7 @@ export const cart: CartItem[] = [
       description: "Short Product Description7",
       id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
       price: 15,
-      title: "ProductName",
+      title: "Wakeboard AquaLite True",
     },
     count: 5,
   },
